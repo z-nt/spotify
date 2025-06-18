f97855312e0492b86f13a3777edea83fed8ba293
kiannosratian <kiannosratian76@gmail.com>
2025-06-19 02:25:47 +0330

    work on static menu

--- 01/spotify/src/staticMenu/components/components.js
+++ 01/spotify/src/staticMenu/components/components.js
@@ -10,3 +10,82 @@ export function PlusButton({onClick}) {
 		</button>
 	)
 };
+
+
+function CreatePlayListButton ({onClick}){
+
+	return (
+		<>
+		<button className="playListButton staticButtons " onClick={onClick}>
+			Create playlist
+		</button>
+		</>
+	)
+}
+
+
+
+
+export function CreatePlayList (){
+	const handelClick = () => {
+		console.log("createList ");
+	}	
+	return (
+		<div className="playListContainer staticTextStyle ">
+			<h4>Create your first playlist </h4>
+			<p>it is easy, we will help you </p>
+
+			<CreatePlayListButton onClick={handelClick}  />
+		</div>	
+	)
+}
+
+
+
+
+
+
+
+function Browsepodcasts ({onClick}){
+
+	return (
+		<>
+		<button className="browseButton staticButtons " onClick={onClick}>
+			Browse podcasts
+		</button>
+		</>
+	)
+}
+
+
+
+
+export function CreateBrowsePadcasts (){
+	const handelClick = () => {
+		console.log("browse ");
+	}	
+	return (
+		<div className="browseContainer staticTextStyle  ">
+			<h4>Lest find some podcasts to follow </h4>
+			<p>we will keep you updated on new episodes </p>
+
+			<Browsepodcasts onClick={handelClick}  />
+		</div>	
+	)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 01/spotify/src/staticMenu/staticMenu.js
+++ 01/spotify/src/staticMenu/staticMenu.js
@@ -1,20 +1,45 @@
 import "./staticMenu.css";
-import {PlusButton} from "./components/components.js";
-
+import {PlusButton,CreatePlayList,CreateBrowsePadcasts} from "./components/components.js";
 function StaticMenu (){
 	return (
 		<div className ="staticMenu" >
 			<div className="headerStaticMenu">
-		
 			 	<h4>Your Library</h4>
-				<PlusButton  />
+				<PlusButton/>
 			</div>
+			<div className="componentsContainer" > 	
+				<CreatePlayList/>
+				<CreateBrowsePadcasts/>
+			</div>
+			<div className="staticFooter">
+				 <ul className="footerItemsList">
+			 		<li className="items">
+					Legal
+					</li>
+			 		<li className="items">
+					Safty & Privacy Center
+					</li>
+			 		<li className="items">
+					Privacy Policy 
+					</li>
+			 		<li className="items">
+					Cookie Setting 
+					</li>
+			 		<li className="items">
+					About Ads 
+					</li>
+			 		<li className="items">
+					Accessibillity 
+					</li>
 		
+			 		<li className="items">
+					Accessibillity 
+					</li>
+				</ul>
+				<h4>Cookies</h4>
+			</div>
 		</div>
-
 	)
-
-
 }
 
 export default StaticMenu;
